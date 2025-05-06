3af0955f42431a3833678a413ad60a1098866b49
Jacqueline Lim <jacquelinelim0513@gmail.com>
2025-05-06 11:53:20 +0800

    importgradient

--- Screens/ForgetPassword.tsx
+++ Screens/ForgetPassword.tsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { ForgetPasswordStyles as styles } from '../Styles';
 import { Image, SafeAreaView, Text, TextInput, TouchableOpacity, View, ScrollView, ActivityIndicator, Alert } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
@@ -7,46 +7,37 @@ import { useTheme } from '../ThemeContext';
 
 import { sendPasswordResetEmail } from 'firebase/auth';
 import { FIREBASE_AUTH } from '../FirebaseConfig';
+import LinearGradient from 'react-native-linear-gradient'; // Import LinearGradient
 
 type Props = StackScreenProps<SignInUpStackParamList, "ForgetPassword">;
 
 const ForgetPassword = ({ route, navigation }: Props) => {
   const { theme } = useTheme();
   const [email, setEmail] = useState('');
-  //const [password, setPassword] = useState('');
-  //const [confirmPassword, setConfirmPassword] = useState('');
   const [loading, setLoading] = useState(false);
 
-
   // handle onPress for password update
-  const handleUpdatePassword = async() => {
+  const handleUpdatePassword = async () => {
     console.log("Updated Password pressed");
-    if(!email){
+    if (!email) {
       Alert.alert("Password cannot be empty.\nPlease enter your password.");
       return;
     }
-    //if(!confirmPassword || !(confirmPassword===password)){
-    //  Alert.alert("Confirm password is not same as your password.\nPlease ensure confirm password is same as your password.");
-    //  return;
-    //}
     setLoading(true);
-    try{
+    try {
       await sendPasswordResetEmail(FIREBASE_AUTH, email);
       Alert.alert("Password reset email sent!", "Please check your inbox.");
       navigation.navigate('SignIn');
-    }catch(error: any){
+    } catch (error: any) {
       console.log("Forget password:" + error);
       if (error.code === 'auth/invalid-email') {
         Alert.alert("Invalid Email", "Please enter a valid email address.");
       } else {
         Alert.alert("Error", error.message);
       }
-
-      
-    }finally{
+    } finally {
       setLoading(false);
     }
-    
   };
 
   const handleSignIn = () => {
@@ -54,103 +45,74 @@ const ForgetPassword = ({ route, navigation }: Props) => {
     navigation.navigate('SignIn');
   };
 
+  // Determine gradient colors based on theme
+  const gradientColors = theme === 'dark'
+    ? ['#333', '#222222', '#333']
+    : ['#F2A7ED', '#FDE6F6', '#D4C3F9'];
+
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: theme === 'dark' ? '#333' : '#FDE6F6' }]}>
-      <ScrollView contentContainerStyle={styles.scrollContainer} keyboardShouldPersistTaps="handled">
-        <View style={styles.innerContainer}>
-          {/* Logo */}
-          <View style={styles.logoContainer}>
-            <Image source={require('../img/logo.jpg')} style={styles.logo} />
-          </View>
-
-          <Text style={[styles.title, { color: theme === 'dark' ? '#fff' : '#000' }]}>
-            Password Reset
-          </Text>
-          <View style={styles.formContainer}>
-            {/* Email */}
-            <Text style={[styles.label, { color: theme === 'dark' ? '#fff' : '#000' }]}>
-              Email
-            </Text>
-            <View style={styles.inputWrapper}>
-              <TextInput
-                style={[styles.input]}
-                placeholder="Enter your email"
-                keyboardType="email-address"
-                autoCapitalize="none"
-                value={email}
-                onChangeText={setEmail}
-                placeholderTextColor={theme === 'dark' ? '#aaa' : '#aaa'}
-              />
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* Apply LinearGradient to wrap the entire screen */}
+      <LinearGradient
+        colors={gradientColors}
+        style={{ flex: 1 }}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+      >
+        <ScrollView contentContainerStyle={styles.scrollContainer} keyboardShouldPersistTaps="handled">
+          <View style={styles.innerContainer}>
+            {/* Logo */}
+            <View style={styles.logoContainer}>
+              <Image source={require('../img/logo.jpg')} style={styles.logo} />
             </View>
 
-            
-            {/* Password 
-            <Text style={[styles.label, { color: theme === 'dark' ? '#fff' : '#000' }]}>
-              Password
+            <Text style={[styles.title, { color: theme === 'dark' ? '#fff' : '#000' }]}>
+              Password Reset
             </Text>
-            <View style={styles.inputWrapper}>
-              <TextInput
-                style={[styles.input]}
-                placeholder="Enter your password"
-                secureTextEntry
-                value={password}
-                onChangeText={setPassword}
-                placeholderTextColor={theme === 'dark' ? '#aaa' : '#aaa'}
-              />
-            </View>
-            */}
-
-            {/* Confirm Password 
-            <Text style={[styles.label, { color: theme === 'dark' ? '#fff' : '#000' }]}>
-              Confirm Password
-            </Text>
-            <View style={styles.inputWrapper}>
-              <TextInput
-                style={[styles.input]}
-                placeholder="Re-enter your password"
-                secureTextEntry
-                value={confirmPassword}
-                onChangeText={setConfirmPassword}
-                placeholderTextColor={theme === 'dark' ? '#aaa' : '#aaa'}
-              />
-            </View>
-            */}
-
-            {/* Update Password Button */}
-            {loading? <ActivityIndicator size="large" color="#0000ff" />
-                                      : <>
-                        
-                                      <TouchableOpacity
-                                        style={styles.button}
-                                        onPress={handleUpdatePassword}
-                                      >
-                                        <Text style={styles.buttonText}>Send reset password link to your email.</Text>
-                                      </TouchableOpacity>
-                                      
-                        </>}
-            {/**             
-            <TouchableOpacity
-              style={styles.button}
-              onPress={handleUpdatePassword}
-            >
-              <Text style={styles.buttonText}>Update Password</Text>
-            </TouchableOpacity>
-            */}
-
-            {/* Already have an account */}
-            <View style={styles.tipsText}>
-              <Text style={{ color: theme === 'dark' ? '#fff' : '#000' }}>
-                Already have an account?
+            <View style={styles.formContainer}>
+              {/* Email */}
+              <Text style={[styles.label, { color: theme === 'dark' ? '#fff' : '#000' }]}>
+                Email
               </Text>
-              <TouchableOpacity onPress={handleSignIn}>
-                <Text style={[styles.tips, { color: theme === 'dark' ? '#fff' : '#000' }]}>
-                  Sign In
+              <View style={styles.inputWrapper}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your email"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  value={email}
+                  onChangeText={setEmail}
+                  placeholderTextColor={theme === 'dark' ? '#aaa' : '#aaa'}
+                />
+              </View>
+
+              {/* Send reset password link */}
+              {loading ? (
+                <ActivityIndicator size="large" color="#0000ff" />
+              ) : (
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={handleUpdatePassword}
+                >
+                  <Text style={styles.buttonText}>Send reset password link to your email.</Text>
+                </TouchableOpacity>
+              )}
+
+              {/* Already have an account */}
+              <View style={styles.tipsText}>
+                <Text style={{ color: theme === 'dark' ? '#fff' : '#000' }}>
+                  Already have an account?
                 </Text>
-              </TouchableOpacity>
+                <TouchableOpacity onPress={handleSignIn}>
+                  <Text style={[styles.tips, { color: theme === 'dark' ? '#fff' : '#FF2EAB' }]}>
+                    Sign In
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </LinearGradient>
     </SafeAreaView>
   );
 };
--- Screens/SignUp.tsx
+++ Screens/SignUp.tsx
@@ -9,21 +9,22 @@ import { FIREBASE_AUTH } from '../FirebaseConfig';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 import { insertUser } from '../SQLite';
+import LinearGradient from 'react-native-linear-gradient'; 
 
 type Props = StackScreenProps<SignInUpStackParamList, "SignUp">;
 
 const SignUp = ({ route, navigation }: Props) => {
   const { theme } = useTheme();
   const [email, setEmail] = useState('');
-  const [ username, setUsername] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const auth = FIREBASE_AUTH;
 
-  const validateEmail = async(email: string): Promise<boolean> => {
+  const validateEmail = async (email: string): Promise<boolean> => {
     try {
-      const url =  `https://emailvalidation.abstractapi.com/v1/?api_key=32bad148cbbc439d81a84df08cad81be&email=${email}`;
+      const url = `https://emailvalidation.abstractapi.com/v1/?api_key=32bad148cbbc439d81a84df08cad81be&email=${email}`;
       const response = await fetch(url);
       const data = await response.json();
 
@@ -37,12 +38,11 @@ const SignUp = ({ route, navigation }: Props) => {
     }
   };
 
-  {/**handle onPress */}
-  const handleSignUp = async() => {
- 
+  // handle onPress
+  const handleSignUp = async () => {
     console.log('Sign Up pressed');
     setLoading(true);
-    
+
     if (!email || !username || !password || !confirmPassword) {
       Alert.alert("All fields are required.");
       setLoading(false);
@@ -50,7 +50,7 @@ const SignUp = ({ route, navigation }: Props) => {
     }
 
     const isValid = await validateEmail(email);
-    
+
     if (!isValid) {
       Alert.alert("Invalid email format. Please enter a valid email address.");
       setLoading(false);
@@ -62,9 +62,8 @@ const SignUp = ({ route, navigation }: Props) => {
       setLoading(false);
       return;
     }
-    
-  
-    try{
+
+    try {
       const response = await createUserWithEmailAndPassword(auth, email, password);
       console.log(response);
       const userID = response.user.uid;
@@ -73,11 +72,10 @@ const SignUp = ({ route, navigation }: Props) => {
       await insertUser(userID, username);
       console.log("Insert new user success. UID:", userID);
       Alert.alert("Sign Up successful.\nPlease check your email.");
-      
-    }catch(error: any){
+    } catch (error: any) {
       console.log(error);
       Alert.alert("Sign Up failed: " + error.message);
-    }finally{
+    } finally {
       setLoading(false);
     }
   };
@@ -87,141 +85,136 @@ const SignUp = ({ route, navigation }: Props) => {
     navigation.navigate("SignIn");
   };
 
+  // Determine gradient colors based on theme
+  const gradientColors = theme === 'dark'
+    ? ['#333', '#222222', '#333']
+    : ['#F2A7ED', '#FDE6F6', '#D4C3F9'];
+
   return (
-    <SafeAreaView
-      style={[
-        styles.container,
-        { backgroundColor: theme === 'dark' ? '#333' : '#FDE6F6' }, 
-      ]}
-    >
-      <ScrollView contentContainerStyle={styles.scrollContainer} keyboardShouldPersistTaps="handled">
-        <View style={styles.innerContainer}>
-          {/* Logo */}
-          <View style={styles.logoContainer}>
-            <Image source={require('../img/logo.jpg')} style={styles.logo} />
-          </View>
-
-          <Text
-            style={[
-              styles.title,
-              { color: theme === 'dark' ? '#fff' : '#000' }, 
-            ]}
-          >
-            Sign Up
-          </Text>
-          <View style={styles.formContainer}>
-
-
-            {/* Email */}
-            <Text
-              style={[styles.label, { color: theme === 'dark' ? '#fff' : '#000' }]} 
-            >
-              Email
-            </Text>
-            <View style={styles.inputWrapper}>
-              <TextInput
-                style={[styles.input]} 
-                placeholder="Enter your Email here"
-                keyboardType="email-address"
-                autoCapitalize="none"
-                value={email}
-                onChangeText={setEmail}
-                placeholderTextColor={theme === 'dark' ? '#aaa' : '#aaa'}
-              />
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* Apply LinearGradient to wrap the entire screen */}
+      <LinearGradient
+        colors={gradientColors}
+        style={{ flex: 1 }}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+      >
+        <ScrollView contentContainerStyle={styles.scrollContainer} keyboardShouldPersistTaps="handled">
+          <View style={styles.innerContainer}>
+            {/* Logo */}
+            <View style={styles.logoContainer}>
+              <Image source={require('../img/logo.jpg')} style={styles.logo} />
             </View>
 
-
-            {/* Username */}
             <Text
-              style={[styles.label, { color: theme === 'dark' ? '#fff' : '#000' }]} 
+              style={[styles.title, { color: theme === 'dark' ? '#fff' : '#000' }]}
             >
-              Username
+              Sign Up
             </Text>
-            <View style={styles.inputWrapper}>
-              <TextInput
-                style={[styles.input]} 
-                placeholder="Enter your Username here"
-                keyboardType="default"
-                autoCapitalize="none"
-                value={username}
-                onChangeText={setUsername}
-                placeholderTextColor={theme === 'dark' ? '#aaa' : '#aaa'}
-              />
-            </View>
+            <View style={styles.formContainer}>
+              {/* Email */}
+              <Text
+                style={[styles.label, { color: theme === 'dark' ? '#fff' : '#000' }]}
+              >
+                Email
+              </Text>
+              <View style={styles.inputWrapper}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your Email here"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  value={email}
+                  onChangeText={setEmail}
+                  placeholderTextColor={theme === 'dark' ? '#aaa' : '#aaa'}
+                />
+              </View>
 
-            {/* Password */}
-            <Text
-              style={[styles.label, { color: theme === 'dark' ? '#fff' : '#000' }]} 
-            >
-              Password
-            </Text>
-            <View style={styles.inputWrapper}>
-              <TextInput
-                style={[styles.input]} 
-                placeholder="Enter your password"
-                secureTextEntry
-                value={password}
-                onChangeText={setPassword}
-                placeholderTextColor={theme === 'dark' ? '#aaa' : '#aaa'}
-              />
-            </View>
+              {/* Username */}
+              <Text
+                style={[styles.label, { color: theme === 'dark' ? '#fff' : '#000' }]}
+              >
+                Username
+              </Text>
+              <View style={styles.inputWrapper}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your Username here"
+                  keyboardType="default"
+                  autoCapitalize="none"
+                  value={username}
+                  onChangeText={setUsername}
+                  placeholderTextColor={theme === 'dark' ? '#aaa' : '#aaa'}
+                />
+              </View>
 
-            {/* Confirm Password */}
-            <Text
-              style={[styles.label, { color: theme === 'dark' ? '#fff' : '#000' }]} 
-            >
-              Confirm Password
-            </Text>
-            <View style={styles.inputWrapper}>
-              <TextInput
-                style={[styles.input]} 
-                placeholder="Re-enter your password"
-                secureTextEntry
-                value={confirmPassword}
-                onChangeText={setConfirmPassword}
-                placeholderTextColor={theme === 'dark' ? '#aaa' : '#aaa'}
-              />
-            </View>
+              {/* Password */}
+              <Text
+                style={[styles.label, { color: theme === 'dark' ? '#fff' : '#000' }]}
+              >
+                Password
+              </Text>
+              <View style={styles.inputWrapper}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your password"
+                  secureTextEntry
+                  value={password}
+                  onChangeText={setPassword}
+                  placeholderTextColor={theme === 'dark' ? '#aaa' : '#aaa'}
+                />
+              </View>
 
-            {/* Sign Up Button */}
-            {loading? <ActivityIndicator size="large" color="#0000ff" />
-                          : <>
-            
-                          <TouchableOpacity
-                            style={styles.button}
-                            onPress={handleSignUp}
-                          >
-                            <Text style={styles.buttonText}>Sign Up</Text>
-                          </TouchableOpacity>
-                          
-            </>}
+              {/* Confirm Password */}
+              <Text
+                style={[styles.label, { color: theme === 'dark' ? '#fff' : '#000' }]}
+              >
+                Confirm Password
+              </Text>
+              <View style={styles.inputWrapper}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Re-enter your password"
+                  secureTextEntry
+                  value={confirmPassword}
+                  onChangeText={setConfirmPassword}
+                  placeholderTextColor={theme === 'dark' ? '#aaa' : '#aaa'}
+                />
+              </View>
 
-            {/** 
-            <TouchableOpacity
-              style={styles.button}
-              onPress={handleSignUp}
-            >
-              <Text style={styles.buttonText}>Sign Up</Text>
-            </TouchableOpacity>
-            */}
+              {/* Sign Up Button */}
+              {loading ? (
+                <ActivityIndicator size="large" color="#0000ff" />
+              ) : (
+                <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+                  <Text style={styles.buttonText}>Sign Up</Text>
+                </TouchableOpacity>
+              )}
 
-            {/* Already have an account */}
-            <View style={styles.tipsText}>
-              <Text style={{ color: theme === 'dark' ? '#fff' : '#000' }}>Already have an account?</Text>
-              <TouchableOpacity onPress={handleSignIn}>
+              {/* Already have an account */}
+              <View style={styles.tipsText}>
                 <Text
-                  style={[
-                    styles.tips,
-                    { color: theme === 'dark' ? '#fff' : '#000' }, 
-                  ]}
+                  style={{
+                    color: theme === 'dark' ? '#fff' : '#000',
+                  }}
                 >
-                  Sign In
+                  Already have an account?
                 </Text>
-              </TouchableOpacity>
+                <TouchableOpacity onPress={handleSignIn}>
+                  <Text
+                    style={[
+                      styles.tips,
+                      { color: theme === 'dark' ? '#fff' : '#FF2EAB' },
+                    ]}
+                  >
+                    Sign In
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </LinearGradient>
     </SafeAreaView>
   );
 };
