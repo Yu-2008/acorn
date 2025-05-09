db66f02cb5876e0227541e33e5550923049d8461
Yu-2008 <yukiwongyuki1025@gmail.com>
2025-05-09 23:13:21 +0800

    restore delete account

--- Screens/Setting.tsx
+++ Screens/Setting.tsx
@@ -13,8 +13,6 @@ import { FIREBASE_AUTH } from "../src/config/FirebaseConfig";
 import { getUsernameById } from "../src/database/database";
 import { useFocusEffect } from "@react-navigation/native";
 import { createMapLink } from 'react-native-open-maps';
-import { deleteUserAccById } from "../src/database/database";
-import { EmailAuthProvider, reauthenticateWithCredential } from "firebase/auth";
 
 
 
@@ -72,22 +70,6 @@ const Setting = ({ navigation }:Props) => {
     }
   };
 
-  const promptForPassword = async (): Promise<string | null> => {
-  return new Promise((resolve, reject) => {
-    Alert.prompt(
-      'Enter your password',
-      'Please enter your password to confirm the deletion',
-      [
-        { text: 'Cancel', 
-          onPress: () => resolve(null), style: 'cancel' },
-        { text: 'OK', 
-          onPress: (password) => resolve(password || null) }
-      ],
-      'secure-text'
-    );
-  });
-};
-
   const handleDeleteAccount = () => {
     Alert.alert(
       "Delete Account",
@@ -102,27 +84,15 @@ const Setting = ({ navigation }:Props) => {
           style: "destructive",
           onPress: async () => {
             const user = FIREBASE_AUTH.currentUser;
-            if (!user || !userID || !user.email) {
+            if (!user) {
               console.warn("No user is currently signed in.");
               return;
             }
   
             try {
-              const password = await promptForPassword();
-
-              if (password) {
-              const credential = EmailAuthProvider.credential(user.email, password);
-              await reauthenticateWithCredential(user, credential); // Re-authenticate the user
-
-              // Now delete the account
               await user.delete();
-              console.log("User account deleted successfully in Firebase.");
-
-              // delete user data from your local database
-              await deleteUserAccById(userID);
-              console.log("User data deleted successfully in local.");
-              }
-
+              console.log("User account deleted successfully.");
+              // Optionally navigate to login or show a success message
             } catch (error: any) {
               if (error.code === 'auth/requires-recent-login') {
                 Alert.alert("Re-authentication Required", "Please sign in again to delete your account.");
